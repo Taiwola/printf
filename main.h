adb181a14804c74674d74d5ff7fe81bd12609729
Taiwola <seunolanitori@gmail.com>
2022-10-16 10:16:42 -0700

    a function that produces output according to a format.

--- main.h
+++ main.h
@@ -6,7 +6,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * struct specifier - struct specifier
+ * @valid: the valid character.
+ * @f: the functions associated.
+ */
+
+typedef struct specifier
+{
+char *valid;
+int (*f)(va_list);
+}spec;
+
 int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int _putchar(char c)
+int print_percent(va_list args);
+int (*get_func(char x))(va_list args);
 
 unsigned int convert_c(va_list args, buffer_t *output, unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_s(va_list args, buffer_t *output, unsigned char flags, int wid, int prec, unsigned char len);
